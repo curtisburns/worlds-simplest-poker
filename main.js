07a44eaf124a115dbbdc8511cd4564e5d56aa33b
curtisburns <curtis.burns28@gmail.com>
2018-10-30 17:22:25 +0000

    added shuffling function

--- main.js
+++ main.js
@@ -4,8 +4,10 @@
 // Add an option to configure how many players and cards dealt;
 // Total up the players hand and declare the winner(s);
 
+////////////////////////////////////////////////////////////////
 
-
+// Global
+const deck = [];
 const suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
@@ -22,17 +24,31 @@ class Card {
 
 //This constructs a deck when a game starts
 function constructDeck() {
-  const deck = [];
   suits.forEach(suit => {
     values.forEach(value => {
-      const inNewCard = new Card(suit,value);
+      const inNewCard = new Card(suit, value);
       deck.push(inNewCard);
     });
   });
-  console.log(deck);
 }
 
-constructDeck();
+// Creates and shuffles deck - fisher-yates shuffle
+function shuffleDeck() {
+  constructDeck();
+
+  // Must decrement in order to maximise shuffle
+  const deckSize = deck.length;
+  for ( let i = deckSize-1; i >= 0; i-- ) {
+    const randomIndex = Math.floor(Math.random() * deckSize);
+    const temp = deck[i];
+    deck[i] = deck[randomIndex];
+    deck[randomIndex] = temp;
+  }
+}
+
+shuffleDeck();
+
+
 
 window.onload = () => {
 
