a89777a9361a1f1f6b0d83d516ed180af8867863
curtisburns <curtis.burns28@gmail.com>
2018-10-30 16:51:21 +0000

    initial setup with a breakdown of what Ill need to achieve - will add one once basics are in place

---
+++ main.js
@@ -0,0 +1,10 @@
+// Construct a deck;
+// Shuffle the deck;
+// Deal cards while also removing them from the deck;
+// Add an option to configure how many players and cards dealt;
+// Total up the players hand and declare the winner(s);
+
+
+window.onload = () => {
+  
+};
