3d57f41e079972dc2fb8d5a6f7d3181d037f52de
curtisburns <curtis.burns28@gmail.com>
2018-10-31 15:38:10 +0000

    added total and determine winner function, need to refactor and add graphics

--- main.js
+++ main.js
@@ -13,19 +13,21 @@ window.onload = () => {
   const downArrow1 = document.getElementsByClassName('down-arrow-1')[0];
   const upArrow2 = document.getElementsByClassName('up-arrow-2')[0];
   const downArrow2 = document.getElementsByClassName('down-arrow-2')[0];
+  const playGameButton = document.getElementsByClassName('play-game')[0];
 
   // Global
-  const deck = [];
   const suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
   const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  let deck = [];
+  let players = [];
 
 
 
   // Initialised at 2 players
   let handSizeOptions = [];
   let playerNumberOptions = [1, 2];
-  const currentPlayerChoice = {number: playerNumberOptions[1]};
-  const currentHandSizeChoice = {number: 6};
+  let currentPlayerChoice = playerNumberOptions[1];
+  let currentHandSizeChoice = 6;
 
   // This class builds the deck, dealt and totalled are intialised as false to be
   // used later
@@ -42,6 +44,7 @@ window.onload = () => {
   class Player {
     constructor() {
       this.hand = [];
+      this.points = 0;
     }
   }
 
@@ -80,7 +83,7 @@ window.onload = () => {
   // versa
   function calculateSizeOfHand() {
     handSizeOptions = [];
-    const maxHandSize = Math.floor(deck.length/currentPlayerChoice.number);
+    const maxHandSize = Math.floor(deck.length/currentPlayerChoice);
     // Determines hand size options
     for ( let i = 1; i <= maxHandSize; i++ ) {
       handSizeOptions[i-1] = i;
@@ -89,7 +92,7 @@ window.onload = () => {
 
   function calculateNumberOfPlayers() {
     playerNumberOptions = [];
-    const maxNumberOfPlayers = Math.floor(deck.length/currentHandSizeChoice.number);
+    const maxNumberOfPlayers = Math.floor(deck.length/currentHandSizeChoice);
     // Determines player number options
     for ( let i = 1; i <= maxNumberOfPlayers; i++ ) {
       playerNumberOptions[i-1] = i;
@@ -97,8 +100,8 @@ window.onload = () => {
   }
 
   function updateDOMOptions() {
-    numberOfPlayersDOM.textContent = currentPlayerChoice.number;
-    sizeOfHandDOM.textContent = currentHandSizeChoice.number;
+    numberOfPlayersDOM.textContent = currentPlayerChoice;
+    sizeOfHandDOM.textContent = currentHandSizeChoice;
   }
 
 
@@ -116,10 +119,10 @@ window.onload = () => {
         calculateNumberOfPlayers();
         calculateSizeOfHand();
 
-        const index = playerNumberOptions.indexOf(currentPlayerChoice.number) + 1;
+        const index = playerNumberOptions.indexOf(currentPlayerChoice) + 1;
         // If the index calculated is higher than possible, send to beginning.
         // Otherwise continue to increase;
-        currentPlayerChoice.number = index > playerNumberOptions.length-1 ? playerNumberOptions[0] : playerNumberOptions[index];
+        currentPlayerChoice = index > playerNumberOptions.length-1 ? playerNumberOptions[0] : playerNumberOptions[index];
 
         updateDOMOptions();
       };
@@ -130,10 +133,10 @@ window.onload = () => {
         calculateNumberOfPlayers();
         calculateSizeOfHand();
 
-        const index = playerNumberOptions.indexOf(currentPlayerChoice.number) - 1;
+        const index = playerNumberOptions.indexOf(currentPlayerChoice) - 1;
         // If the index calculated is lower than possible, send to end.
         // Otherwise continue to decrease;
-        currentPlayerChoice.number = index < 0 ? playerNumberOptions[playerNumberOptions.length-1] : playerNumberOptions[index];
+        currentPlayerChoice = index < 0 ? playerNumberOptions[playerNumberOptions.length-1] : playerNumberOptions[index];
 
         updateDOMOptions();
       };
@@ -147,10 +150,10 @@ window.onload = () => {
         calculateSizeOfHand();
         calculateNumberOfPlayers();
 
-        const index = handSizeOptions.indexOf(currentHandSizeChoice.number) + 1;
+        const index = handSizeOptions.indexOf(currentHandSizeChoice) + 1;
         // If the index calculated is higher than possible, send to beginning.
         // Otherwise continue to increase;
-        currentHandSizeChoice.number = index > handSizeOptions.length-1 ? handSizeOptions[0] : handSizeOptions[index];
+        currentHandSizeChoice = index > handSizeOptions.length-1 ? handSizeOptions[0] : handSizeOptions[index];
 
         updateDOMOptions();
       };
@@ -161,15 +164,78 @@ window.onload = () => {
         calculateSizeOfHand();
         calculateNumberOfPlayers();
 
-        const index = handSizeOptions.indexOf(currentHandSizeChoice.number) - 1;
+        const index = handSizeOptions.indexOf(currentHandSizeChoice) - 1;
         // If the index calculated is lower than possible, send to end.
         // Otherwise continue to decrease;
-        currentHandSizeChoice.number = index < 0 ? handSizeOptions[handSizeOptions.length-1] : handSizeOptions[index];
+        currentHandSizeChoice = index < 0 ? handSizeOptions[handSizeOptions.length-1] : handSizeOptions[index];
 
         updateDOMOptions();
       };
     }
   }
 
+  ////////////////// START GAME ///////////////////
+  function dealCards() {
+    createPlayers();
+    let card = 0;
+    for ( let i = 0; i < currentHandSizeChoice; i++ ) {
+      for( let j = 0; j < players.length; j++ ) {
+        players[j].hand.push(deck[card]);
+        deck[card].dealt = true;
+        card++;
+      }
+    }
+    console.log('players', players);
+    deck = deck.filter(card => card.dealt !== true );
+    console.log('deck', deck);
+  }
 
+  function createPlayers() {
+    players = [];
+    console.log(currentPlayerChoice);
+    for ( let i = 0; i < currentPlayerChoice; i++) {
+      const newPlayer = new Player();
+      players.push(newPlayer);
+    }
+  }
+
+  dealCards();
+
+  function determineWinner() {
+    const result = {
+      player: [],
+      score: 0
+    };
+    players.reduce((acc, player, index) => {
+
+      if (totalHand(player) > result.score) {
+        acc.score = totalHand(player);
+        acc.player = [index+1];
+        return acc;
+      }
+
+      if (totalHand(player) === result.score) {
+        acc.player.push(index+1);
+      }
+    }, result);
+    console.log(result.player.length);
+    if (result.player.length > 1) {
+      console.log('Drawer', result.player.join('player '), result.score);
+    } else {
+      console.log('Winner!', result.player);
+    }
+  }
+
+  function totalHand(player) {
+    return player.hand.reduce((acc, card) => {
+      card.totalled = true;
+      const total = acc + card.value;
+      return total;
+    }, 0);
+  }
+
+  determineWinner();
+
+  playGameButton.addEventListener('click', shuffleDeck);
+  shuffleDeck();
 };
