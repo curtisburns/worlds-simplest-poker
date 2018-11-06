5396685dd8c99328596ef21a5e3e676299405620
curtisburns <curtis.burns28@gmail.com>
2018-11-06 23:35:44 +0000

    work around for adding static site to heroku

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
